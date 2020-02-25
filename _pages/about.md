64f10bde7641627fec21385fcbec5976fff134a2
tania <taniadaniela1803@gmail.com>
2020-02-25 07:45:03 -0300

    V1 ready.

---
+++ _pages/about.md
@@ -0,0 +1,15 @@
+---
+permalink: /about/
+title: "About"
+---
+
+This site was built using Jekyll with Minimal Mistakes theme. 
+
+You can find the source code for Minimal Mistakes at GitHub:
+[minimal](https://github.com/mmistakes/minimal-mistakes)
+
+You can find the source code for Jekyll at GitHub:
+[jekyll](https://github.com/jekyll/jekyll)
+
+
+[jekyll-organization]: https://github.com/jekyll
---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,6 @@
+var store = [{
+        "title": "How I end up \"reading\" more",
+        "excerpt":"One of my goals for 2019 was to read more, my idea was to read at least one book per month. I failed at this goal, and honestly I failed fantastically, because I started to read 5 books, and I couldn’t finished a single one. My reading list of articles...","categories": ["books"],
+        "tags": ["reading","pocket","kindle"],
+        "url": "http://localhost:4000/books/reading/",
+        "teaser":null}]
